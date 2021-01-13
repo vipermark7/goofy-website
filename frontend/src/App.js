8a19fca4c17faeb91b81bc9b3166f0ccecd7b9df
Shaffan <spacey@pop-os.localdomain>
2021-01-13 10:43:45 -0500

    counter works now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom'
+
 class Button extends React.Component {
-  getInitialState() {
-    return {
+  constructor(props) {
+    super(props);
+    this.state = {
       counter: 0
     };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
   increment() {
     this.setState({ counter: this.state.counter + 1 });
@@ -14,12 +17,12 @@ class Button extends React.Component {
     this.setState({ counter: this.state.counter - 1 });
   }
   render() {
-    return ( 
+    return (
       <div>
         <div id='counter'>{this.state.counter}</div>
         <button onClick={this.increment}> Add 1 </button>
         <button onClick={this.decrement}> Minus 1 </button>
-    </div>
+      </div>
     )
   }
 }
@@ -28,11 +31,13 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Shelby and Shaffan's goofy website!</h1>
-      
+
+      <p>Bet you can't click this button a million times!</p>
+      <Button />
     </div>
   );
-  
+
 }
-ReactDOM.render(<Button counter = 0/>, document.getElementById("root"));
+
 
 export default App;
